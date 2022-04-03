e9a7e31af5c38632511a4d8ee9cb821bbe551404
HaniOlwan <heenoow@gmail.com>
2022-04-03 17:01:45 +0300

    test edit product Relates #6

--- src/controllers/editProduct.js
+++ src/controllers/editProduct.js
@@ -1,21 +1,20 @@
-const { deleteProductQuery } = require('../database/queries');
+const { editProductValidation } = require('../utils/validation');
+const { editProductQuery } = require('../database/queries/editProductQuery');
 const { customizedError } = require('../utils');
-const { deleteProductValidation } = require('../utils/validation');
 
-const deleteProduct = (req, res, next) => {
-  deleteProductValidation(req.query)
-    .then(() => deleteProductQuery(req.query))
+const editProduct = (req, res, next) => {
+  editProductValidation(req.body)
+    .then(() => editProductQuery(req.body))
     .then(({ rowCount }) => {
       if (rowCount === 0) {
         res.status(400).json(customizedError(400, 'Bad Request'));
       } else {
         res.json({
           status: 200,
-          message: 'Deleted Successfully',
+          message: 'Updated Successfully',
         });
       }
-    })
-    .catch((err) => {
+    }).catch((err) => {
       if (err.details) {
         res.status(400).json(customizedError(400, err.details[0].message));
       } else {
@@ -24,4 +23,4 @@ const deleteProduct = (req, res, next) => {
     });
 };
 
-module.exports = { deleteProduct };
+module.exports = { editProduct };
