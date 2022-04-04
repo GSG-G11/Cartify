a40306d02cdc0505a3d11de8c94a3df63125d33a
HaniOlwan <heenoow@gmail.com>
2022-04-04 15:56:22 +0300

    make home component to class component Relates #21

--- client/src/components/home/Cards.jsx
+++ client/src/components/home/Cards.jsx
@@ -4,15 +4,15 @@ import '../../stylesheet/Home.css';
 
 const Cards = ({ list }) => (
  <section className="landscape">
-     {
+    {
     list.map((item) => (
-            <div key={1} className="card">
+            <div key={item.id} className="card">
                 <img src={item.img} alt="Image Error" />
                 <h2>{item.title}</h2>
                 <span className="category">{item.category}</span>
                 <h3>{item.price}</h3>
-                <button className="button" onClick={console.log('Add to cart')}>Add to Cart</button>
-            </div>))
+                <button className="button" onClick={(e) => { e.preventDefault(); console.log('Hello From card'); }}>Add to Cart</button>
+        </div>))
      }
 
 </section>
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,54 +2,79 @@ import React from 'react';
 import '../../stylesheet/Home.css';
 import Cards from './Cards.jsx';
 
-function Home() {
-  const items = [
-    {
-      id: 0, img: 'this image', title: 'this title', category: 'this category', price: 'this price',
-    },
-    {
-      id: 1, img: 'this image 1', title: 'this title 1', category: 'this category 1', price: 'this price 1',
-    },
-    {
-      id: 2, img: 'this image 2', title: 'this title 2', category: 'this category 2', price: 'this price 2',
-    },
-  ];
+const items = [
+  {
+    id: 0, img: 'this image', title: 'this title', category: 'Children', price: 'this price',
+  },
+  {
+    id: 1, img: 'this image 1', title: 'this title 1', category: 'Men', price: 'this price 1',
+  },
+  {
+    id: 2, img: 'this image 2', title: 'this title 2', category: 'Women', price: 'this price 2',
+  },
+  {
+    id: 3, img: 'this image 3', title: 'this title 3', category: 'Men', price: 'this price 3',
+  },
+  {
+    id: 4, img: 'this image 4', title: 'this title 4', category: 'all', price: 'this price 4',
+  },
+  {
+    id: 5, img: 'this image 5', title: 'this title 5', category: 'Women', price: 'this price 5',
+  },
+];
 
-  return (
-<div>
-    <nav>
-        <h1 className="logo">Cartify</h1>
+class Home extends React.Component {
+  state = {
+    category: '',
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+  filterCategory(event) {
+    event.preventDefault();
+    // this.state.category = event.target;
+    console.log(event.target);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
         <div>
-            <ul>
-                <li ><a href="" className="nav_selected">Home</a></li>
-                <li ><a href="">Products</a></li>
-                <li ><a href="">Cart</a></li>
-            </ul>
-        </div>
-        <button className="button">Login</button>
-    </nav>
-    <section className="header_section">
-        <h1 className="header_title">Our Products</h1>
-        <div className="filter_items">
-            <ul className="filter_category">
-                <li><a href="" className="nav_selected">All</a></li>
-                <li><a href="">Men</a></li>
-                <li><a href="">Women</a></li>
-                <li><a href="">Children</a></li>
-            </ul>
-            <label htmlFor="price_filter" className="filter_label">Filter by price:</label>
-            <select name="price_filter" id="price_filter">
-                <option value="high">High to Low</option>
-                <option value="low">Low to High</option>
-            </select>
+            <nav>
+                <h1 className="logo">Cartify</h1>
+                <div>
+                    <ul>
+                        <li ><a href="" className="nav_selected">Home</a></li>
+                        <li ><a href="">Products</a></li>
+                        <li ><a href="">Cart</a></li>
+                    </ul>
+                </div>
+                <button className="button">Login</button>
+            </nav>
+            <section className="header_section">
+                <h1 className="header_title">Our Products</h1>
+                <div className="filter_items">
+                    <ul className="filter_category">
+                        <li><a href="" value = {'all'} className="nav_selected" onClick={this.filterCategory}>All</a></li>
+                        <li><a href="">Men</a></li>
+                        <li><a href="">Women</a></li>
+                        <li><a href="">Children</a></li>
+                    </ul>
+                    <label htmlFor="price_filter" className="filter_label">Filter by price:</label>
+                    <select name="price_filter" id="price_filter">
+                        <option value="high">High to Low</option>
+                        <option value="low">Low to High</option>
+                    </select>
 
-            <label htmlFor="search_filter" className="filter_label">Search item:</label>
-            <input className="search" type="text" name="" id="search_filter" placeholder="Search item" />
+                    <label htmlFor="search_filter" className="filter_label">Search item:</label>
+                    <input className="search" type="text" name="" id="search_filter" placeholder="Search item" />
+                </div>
+            </section>
+                <Cards list={
+                    this.state.category === '' ? items : items.filter((item) => item.category === this.state.category)
+                } />
         </div>
-    </section>
-        <Cards list={items} />
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
