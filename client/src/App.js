66256b37f89e8fee6693dbf48c915f777aa2d019
farah2003 <farah.shaqoura@example.com>
2022-04-04 13:24:47 +0300

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import NavBar from './component/NavBar';
 
 function App() {
