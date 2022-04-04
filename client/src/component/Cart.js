7cbdcd63a41aafaea86a26de5b78ca77e405496d
farah2003 <farah.shaqoura@example.com>
2022-04-04 17:14:23 +0300

    cart

--- client/src/component/Cart.js
+++ client/src/component/Cart.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import './cart.css';
+import img from '../image/img.png';
 
 function Cart() {
   return (
-        <div>
+    <section className="landscape">
 
-        </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+
+      </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+      </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+
+      </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+
+      </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+
+      </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+
+      </div>
+      <div className="card">
+        <img src={img} alt="Image Error" />
+        <h2>T-Shirt</h2>
+        <span className="category">Children</span>
+        <h3>30$</h3>
+
+      </div>
+
+    </section>
   );
 }
 
