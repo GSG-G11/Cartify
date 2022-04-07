1ba1db23ff46681e5a345e4805a06293de8b35eb
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-04-07 05:14:20 +0300

    deploy

--- src/app.js
+++ src/app.js
@@ -17,10 +17,10 @@ app.use('/api/v1/product', router);
 
 const { NODE_ENV } = process.env;
 
-if (NODE_ENV === 'production') {
-  app.use(express.static(join(__dirname, '..', 'client', 'build')));
-  app.get('*', serveRoot);
-}
+// if (NODE_ENV === 'production') {
+app.use(express.static(join(__dirname, '..', 'client', 'build')));
+app.get('*', serveRoot);
+// }
 
 app.set('port', process.env.PORT || 3001);
 
